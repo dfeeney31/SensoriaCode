718319cfca8109dda00289c33ac399edba872621
Dan Feeney <daniel.feeney@colorado.edu>
2019-04-02 15:17:52 -0600

    Fixed ordering of lace and LR

--- group_analysis.m
+++ group_analysis.m
@@ -14,7 +14,7 @@ for file = files'
 
     [pks_nav, locs_nav] = findpeaks(walk_dat.CS3(10:end), 'MinPeakDistance',10, 'MinPeakHeight',4.2);
     [pks_cub, locs_cub] = findpeaks(walk_dat.CS1(10:end), 'MinPeakDistance',10, 'MinPeakHeight',5.1);
-    [pks_5mt, locs_5mt] = findpeaks(walk_dat.CS1(10:end), 'MinPeakDistance',10, 'MinPeakHeight',5.5);
+    [pks_5mt, locs_5mt] = findpeaks(walk_dat.CS1(10:end), 'MinPeakDistance',10, 'MinPeakHeight',4);
     [pks_1mt, locs_1mt] = findpeaks(walk_dat.CS4(10:end), 'MinPeakDistance',10, 'MinPeakHeight',2);
     [pks_calc, locs_calc] = findpeaks(walk_dat.CS5(10:end), 'MinPeakDistance',10, 'MinPeakheight',12);
     
@@ -22,7 +22,7 @@ for file = files'
     pks_1mt = pks_1mt(pks_1mt > (0.4 * mean(pks_1mt)));
     pks_5mt = pks_5mt(pks_5mt > (0.4 * mean(pks_5mt)));
     pks_calc = pks_calc(pks_calc > (0.4 * mean(pks_calc)));
-    pks_1mt = pks_1mt(pks_1mt > (0.4 * mean(pks_1mt)));
+    pks_cub = pks_cub(pks_cub > (0.4 * mean(pks_cub)));
     
     %%%%% Calculate the averages, etc. %%%
     longdata(1).avg_nav{counter} = mean(pks_nav);
@@ -41,5 +41,74 @@ for file = files'
     longdata(1).nav{counter} = walk_dat.CS3;
     longdata(1).cub{counter} = walk_dat.CS7;
 
+    longdata(1).pkCalc{counter} = pks_calc;
+    longdata(1).pk5mt{counter} = pks_5mt;
+    longdata(1).pk1mt{counter} = pks_1mt;
+    longdata(1).pkCub{counter} = pks_cub;
+    longdata(1).pkNav{counter} = pks_nav;
+    
     counter = counter + 1;
 end
+
+h1 = longdata.pk1mt{1,1}; %L
+h2 = longdata.pk1mt{1,2}; %R
+h3 = longdata.pk1mt{1,3}; %T
+h4 = longdata.pk1mt{1,4}; %X
+%%% Requires brewermap and histf (modified on line 112) in same folder
+map = brewermap(3,'Set1'); 
+figure(1)
+histf(h1,mean(h1)-10*std(h1):.1:mean(h1)+10*std(h1),'facecolor',map(1,:),'facealpha',.5,'edgecolor','none')
+title('1st Met Histogram')
+hold on
+histf(h2,mean(h1)-10*std(h1):.1:mean(h1)+10*std(h1),'facecolor',map(2,:),'facealpha',.5,'edgecolor','none')
+histf(h3,mean(h1)-10*std(h1):.1:mean(h1)+10*std(h1),'facecolor',map(3,:),'facealpha',.5,'edgecolor','none')
+histf(h4,mean(h1)-10*std(h1):.1:mean(h1)+10*std(h1),'facecolor',map(3,:),'facealpha',.5,'edgecolor','none')
+box off
+axis tight
+legend1 = sprintf('LR mu = %.3f', mean(h1));
+legend2 = sprintf('Lace mu = %.3f', mean(h2));
+legend3 = sprintf('Tri mu = %.3f', mean(h3));
+legend4 = sprintf('X mu = %.3f', mean(h4));
+legend({legend1, legend2, legend3, legend4});
+
+%%%%% 5th met %%%%%
+h1 = longdata.pk5mt{1,1}; %L
+h2 = longdata.pk5mt{1,2}; %R
+h3 = longdata.pk5mt{1,3}; %T
+h4 = longdata.pk5mt{1,4}; %X
+map = brewermap(3,'Set1'); 
+figure(2)
+histf(h1,mean(h1)-10*std(h1):.1:mean(h1)+10*std(h1),'facecolor',map(1,:),'facealpha',.5,'edgecolor','none')
+title('5th Met Histograms')
+hold on
+histf(h2,mean(h1)-10*std(h1):.1:mean(h1)+10*std(h1),'facecolor',map(2,:),'facealpha',.5,'edgecolor','none')
+histf(h3,mean(h1)-10*std(h1):.1:mean(h1)+10*std(h1),'facecolor',map(3,:),'facealpha',.5,'edgecolor','none')
+histf(h4,mean(h1)-10*std(h1):.1:mean(h1)+10*std(h1),'facecolor',map(3,:),'facealpha',.5,'edgecolor','none')
+box off
+axis tight
+legend1 = sprintf('LR mu = %.3f', mean(h1));
+legend2 = sprintf('Lace mu = %.3f', mean(h2));
+legend3 = sprintf('Tri mu = %.3f', mean(h3));
+legend4 = sprintf('X mu = %.3f', mean(h4));
+legend({legend1, legend2, legend3, legend4});
+
+%%%%% Heel %%%%%
+h1 = longdata.pkCalc{1,1}; %L
+h2 = longdata.pkCalc{1,2}; %R
+h3 = longdata.pkCalc{1,3}; %T
+h4 = longdata.pkCalc{1,4}; %X
+map = brewermap(3,'Set1'); 
+figure(2)
+histf(h1,mean(h1)-10*std(h1):.5:mean(h1)+10*std(h1),'facecolor',map(1,:),'facealpha',.5,'edgecolor','none')
+title('Calcaneous Histograms')
+hold on
+histf(h2,mean(h1)-10*std(h1):.5:mean(h1)+10*std(h1),'facecolor',map(2,:),'facealpha',.5,'edgecolor','none')
+histf(h3,mean(h1)-10*std(h1):.5:mean(h1)+10*std(h1),'facecolor',map(3,:),'facealpha',.5,'edgecolor','none')
+histf(h4,mean(h1)-10*std(h1):.5:mean(h1)+10*std(h1),'facecolor',map(3,:),'facealpha',.5,'edgecolor','none')
+box off
+axis tight
+legend1 = sprintf('LR mu = %.3f', mean(h1));
+legend2 = sprintf('Lace mu = %.3f', mean(h2));
+legend3 = sprintf('Tri mu = %.3f', mean(h3));
+legend4 = sprintf('X mu = %.3f', mean(h4));
+legend({legend1, legend2, legend3, legend4});
--- makePlots2.m
+++ makePlots2.m
@@ -1,75 +1,74 @@
-function makePlots = makePlots2(data,len,sub)
-% makePlot Builds a shaded error plot for each subject (1,2,3 for Dave,
-% Joe, John, respectively). Len is the length of the slice you'd like. 
-% Len = 125works well. 
+function makePlots = makePlots2(longdata,len,sub)
+% makePlot Builds an average plot for each subject 
+locs = longdata.locs{1,1};
+locs2 = longdata.locs{1,2};
+locs3 = longdata.locs{1,3};
+locs4 = longdata.locs{1,4};
 
-locs = data.locs{1,1};
-locs2 = data.locs{1,2};
-locs3 = data.locs{1,3};
-locs4 = data.locs{1,4};
+calc_lr = zeros(length(longdata.locs{1,1}),(len*2)+1);
+calc_lace = zeros(length(longdata.locs{1,2}),(len*2)+1);
+calc_t = zeros(length(longdata.locs{1,3}),(len*2)+1);
+calc_x = zeros(length(longdata.locs{1,4}),(len*2)+1);
 
-calc_lace = zeros(length(data.locs{1,1}),(len*2)+1);
-calc_lr = zeros(length(data.locs{1,2}),(len*2)+1);
-calc_t = zeros(length(data.locs{1,3}),(len*2)+1);
-calc_x = zeros(length(data.locs{1,4}),(len*2)+1);
+mp1_lr = zeros(length(longdata.locs{1,1}),(len*2)+1);
+mp1_lace = zeros(length(longdata.locs{1,2}),(len*2)+1);
+mp1_t = zeros(length(longdata.locs{1,3}),(len*2)+1);
+mp1_x = zeros(length(longdata.locs{1,4}),(len*2)+1);
 
-mp1_lace = zeros(length(data.locs{1,1}),(len*2)+1);
-mp1_lr = zeros(length(data.locs{1,2}),(len*2)+1);
-mp1_t = zeros(length(data.locs{1,3}),(len*2)+1);
-mp1_x = zeros(length(data.locs{1,4}),(len*2)+1);
-
-mp5_lace = zeros(length(data.locs{1,1}),(len*2)+1);
-mp5_lr = zeros(length(data.locs{1,2}),(len*2)+1);
-mp5_t = zeros(length(data.locs{1,3}),(len*2)+1);
-mp5_x = zeros(length(data.locs{1,4}),(len*2)+1);
+mp5_lr = zeros(length(longdata.locs{1,1}),(len*2)+1);
+mp5_lace = zeros(length(longdata.locs{1,2}),(len*2)+1);
+mp5_t = zeros(length(longdata.locs{1,3}),(len*2)+1);
+mp5_x = zeros(length(longdata.locs{1,4}),(len*2)+1);
 
 
 for i = 2:(length(locs)-2)
-   calc_lace(i,:) =  data.calc{1,sub}(floor(locs(i)-len):floor(locs(i)+len));
-   calc_lr(i,:) =  data.calc{1,sub+1}(floor(locs2(i)-len):floor(locs2(i)+len));
-   calc_t(i,:) =  data.calc{1,sub+2}(floor(locs3(i)-len):floor(locs3(i)+len));
-   calc_x(i,:) =  data.calc{1,sub+3}(floor(locs4(i)-len):floor(locs4(i)+len));
+   calc_lace(i,:) =  longdata.calc{1,sub}(floor(locs(i)-len):floor(locs(i)+len));
+   calc_lr(i,:) =  longdata.calc{1,sub+1}(floor(locs2(i)-len):floor(locs2(i)+len));
+   calc_t(i,:) =  longdata.calc{1,sub+2}(floor(locs3(i)-len):floor(locs3(i)+len));
+   calc_x(i,:) =  longdata.calc{1,sub+3}(floor(locs4(i)-len):floor(locs4(i)+len));
 
-   mp1_lace(i,:) =  data.mtp1{1,sub}(floor(locs(i)-len):floor(locs(i)+len));
-   mp1_lr(i,:) =  data.mtp1{1,sub+1}(floor(locs2(i)-len):floor(locs2(i)+len));
-   mp1_t(i,:) =  data.mtp1{1,sub+2}(floor(locs3(i)-len):floor(locs3(i)+len));
-   mp1_x(i,:) =  data.mtp1{1,sub+3}(floor(locs4(i)-len):floor(locs4(i)+len));
+   mp1_lace(i,:) =  longdata.mtp1{1,sub}(floor(locs(i)-len):floor(locs(i)+len));
+   mp1_lr(i,:) =  longdata.mtp1{1,sub+1}(floor(locs2(i)-len):floor(locs2(i)+len));
+   mp1_t(i,:) =  longdata.mtp1{1,sub+2}(floor(locs3(i)-len):floor(locs3(i)+len));
+   mp1_x(i,:) =  longdata.mtp1{1,sub+3}(floor(locs4(i)-len):floor(locs4(i)+len));
    
-   mp5_lace(i,:) =  data.mtp5{1,sub}(floor(locs(i)-len):floor(locs(i)+len));
-   mp5_lr(i,:) = data.mtp5{1,sub+1}(floor(locs2(i)-len):floor(locs2(i)+len));
-   mp5_t(i,:) =  data.mtp5{1,sub+2}(floor(locs3(i)-len):floor(locs3(i)+len));
-   mp5_x(i,:) =  data.mtp5{1,sub+3}(floor(locs4(i)-len):floor(locs4(i)+len));
+   mp5_lace(i,:) =  longdata.mtp5{1,sub}(floor(locs(i)-len):floor(locs(i)+len));
+   mp5_lr(i,:) = longdata.mtp5{1,sub+1}(floor(locs2(i)-len):floor(locs2(i)+len));
+   mp5_t(i,:) =  longdata.mtp5{1,sub+2}(floor(locs3(i)-len):floor(locs3(i)+len));
+   mp5_x(i,:) =  longdata.mtp5{1,sub+3}(floor(locs4(i)-len):floor(locs4(i)+len));
 end
 
 figure(1)
+plot(mean(calc_lace,1), '-r');
 title('Calcaneous pressure')
-shadedErrorBar(1:length(calc_lace), calc_lace, {@mean,@std}, 'lineprops','-r');
 hold on
-shadedErrorBar(1:length(calc_lr), calc_lr, {@mean,@std}, 'lineprops','-b');
-shadedErrorBar(1:length(calc_t), calc_t, {@mean,@std}, 'lineprops','-g');
-shadedErrorBar(1:length(calc_x), calc_x, {@mean,@std}, 'lineprops','-k');
+plot( mean(calc_lr,1), '-b');
+plot(mean(calc_t,1), '-g');
+plot( mean(calc_x,1), '-k');
 xlabel('Time)','FontSize',14,'FontWeight','bold')
 ylabel('Pressure (PSI)','FontSize',14,'FontWeight','bold')
+legend('LR','Lace','Tri','X')
 
 figure(2)
-title('Y force')
-shadedErrorBar(1:length(mp1_lace), mp1_lace, {@mean,@std}, 'lineprops','-r');
+plot(mean(mp1_lace,1),'-r');
+title('1st Met Pressure')
 hold on
-shadedErrorBar(1:length(mp1_lr), mp1_lr, {@mean,@std}, 'lineprops','-b');
-shadedErrorBar(1:length(mp1_t), mp1_t, {@mean,@std}, 'lineprops','-g');
-shadedErrorBar(1:length(mp1_x), mp1_x, {@mean,@std}, 'lineprops','-k');
+plot( mean(mp1_lr,1), '-b');
+plot(mean(mp1_t,1), '-g');
+plot(mean(mp1_x,1), '-k');
 xlabel('Time (ms)','FontSize',14,'FontWeight','bold')
 ylabel('Force (N)','FontSize',14,'FontWeight','bold')
+legend('LR','Lace','Tri','X')
 
 figure(3)
-title('X force')
-shadedErrorBar(1:length(mp5_lace), mp5_lace, {@mean,@std}, 'lineprops','-r');
+plot(mean(mp5_lace,1), '-r');
+title('5th Met pressure')
 hold on
-shadedErrorBar(1:length(mp5_lr), mp5_lr, {@mean,@std}, 'lineprops','-b');
-shadedErrorBar(1:length(mp5_t), mp5_t, {@mean,@std}, 'lineprops','-g');
-shadedErrorBar(1:length(mp5_x), mp5_x, {@mean,@std}, 'lineprops','-k');
+plot(mean(mp5_lr,1), '-b');
+plot(mean(mp5_t,1), '-g');
+plot(mean(mp5_x,1), '-k');
 xlabel('Time (ms)','FontSize',14,'FontWeight','bold')
 ylabel('Force (N)','FontSize',14,'FontWeight','bold')
-
+legend('LR','Lace','Tri','X')
 end
 
