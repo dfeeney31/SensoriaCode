ae7ed53dc915517d6ff32b2d035bd1b7dea83a82
Dan Feeney <daniel.feeney@colorado.edu>
2019-11-26 09:58:33 -0700

    Adding in comparison of two models for 1500

--- RunDownhill.m
+++ RunDownhill.m
@@ -1,61 +1,61 @@
 clear
-cd 'C:\Users\Daniel.Feeney\Dropbox (Boa)\Trail Run Internal Pilot\TreadmillSensoria\BD\DH'; %Change this line to the subject folder
+cd 'C:\Users\Daniel.Feeney\Dropbox (Boa)\Trail Run Internal Pilot\TreadmillSensoria\NM\DH'; %Change this line to the subject folder
 files = dir('*.csv');
 longdata(1,1) = struct();
 counter = 1;
 addpath('C:\Users\Daniel.Feeney\Desktop\SensoriaCode')  
-leftCal = importfile('C:\Users\Daniel.Feeney\Dropbox (Boa)\Trail Run Internal Pilot\TreadmillSensoria\BD\BD_cal.csv');
+leftCal = importfile('C:\Users\Daniel.Feeney\Dropbox (Boa)\SensoriaFolder\1500Testing\KateCal.csv');
 
 for file = files'
-    walk_dat = importfile(file.name);
-    walk_trial = convLeftVals(walk_dat, leftCal);
-    
-    clear pks_nav locs_nav pks_cub locs_cub pks_5mt locs_5mt pks_1mt locs_1mt pks_calc locs_calc
-
-    %0: Lat 5th side, 1: 5th MTP, 2: 5th toe, 3:1st MTP, 4: navicular, 5:
-    %calcaneous, 6: Hallux, 7: Cuboid
-    
-    [pks_5lat, locs_5late] = findpeaks(walk_trial.CS0(10:end), 'MinPeakDistance',20, 'MinPeakHeight',5);
-    [pks_5mtp, locs_cub] = findpeaks(walk_trial.CS1(10:end), 'MinPeakDistance',20, 'MinPeakHeight',5);
-    [pks_5toe, locs_5mt] = findpeaks(walk_trial.CS2(10:end), 'MinPeakDistance',20, 'MinPeakHeight',5);
-    [pks_1mtp, locs_1mt] = findpeaks(walk_trial.CS3(10:end), 'MinPeakDistance',20, 'MinPeakHeight',5);
-    [pks_nav, locs_nav] = findpeaks(walk_trial.CS4(10:end), 'MinPeakDistance',20, 'MinPeakheight',5);
-    [pks_calc, locs_5mt] = findpeaks(walk_trial.CS5(10:end), 'MinPeakDistance',20, 'MinPeakHeight',5);
-    [pks_hallux, locs_1mt] = findpeaks(walk_trial.CS6(10:end), 'MinPeakDistance',20);
-    [pks_cub, locs_calc] = findpeaks(walk_dat.CS7(10:end), 'MinPeakDistance',20, 'MinPeakheight',5);
-    
-    % Outlier detection %
-    pks_1mtp = pks_1mtp(pks_1mtp > (0.55 * mean(pks_1mtp)));
-    pks_5mtp = pks_5mtp(pks_5mtp > (0.55 * mean(pks_5mtp)));
-    pks_calc = pks_calc(pks_calc > (0.55 * mean(pks_calc)));
-    pks_nav = pks_nav(pks_nav > (0.55 * mean(pks_nav)));
-    
-    %%%%% Calculate the averages, etc. %%%
-    longdata(1).avg_nav{counter} = mean(pks_nav);
-    longdata(1).avg_cub{counter} = mean(pks_cub);
-    longdata(1).avg_5mtp{counter} = mean(pks_5mtp); 
-    longdata(1).avg_1mtp{counter} = mean(pks_1mtp); 
-    longdata(1).avg_heel{counter} = mean(pks_calc);
-    longdata(1).cvHeel{counter} = (std(walk_dat.CS5(10:end)) / mean(walk_dat.CS5(10:end))) * 100;
-    
-    %%% Save TS data %%%
-    longdata(1).locs{counter} = locs_calc;
-    longdata(1).calc{counter} = walk_trial.CS5;
-    longdata(1).mtp1{counter} = walk_trial.CS3;
-    longdata(1).mtp5{counter} = walk_trial.CS1;
-    longdata(1).nav{counter} = walk_trial.CS4;
-    longdata(1).cub{counter} = walk_trial.CS7;
-
-    longdata(1).pkCalc{counter} = pks_calc;
-    longdata(1).pk5mt{counter} = pks_5mtp;
-    longdata(1).pk1mt{counter} = pks_1mtp;
-    longdata(1).pkCub{counter} = pks_nav;
-    longdata(1).pkNav{counter} = pks_nav;
-    longdata(1).config{counter} = file.name;
-    
-    disp(file.name)
-    counter = counter + 1;
-    
+    try
+        walk_dat = importfile(file.name);
+        walk_trial = convLeftVals(walk_dat, leftCal);
+        
+        clear pks_nav locs_nav pks_cub locs_cub pks_5mt locs_5mt pks_1mt locs_1mt pks_calc locs_calc
+        
+        %0: Lat 5th side, 1: 5th MTP, 2: 5th toe, 3:1st MTP, 4: navicular, 5:
+        %calcaneous, 6: Hallux, 7: Cuboid
+        
+        [pks_5lat, locs_5late] = findpeaks(walk_trial.CS0(10:end), 'MinPeakDistance',20, 'MinPeakHeight',5);
+        [pks_5mtp, locs_cub] = findpeaks(walk_trial.CS1(10:end), 'MinPeakDistance',20, 'MinPeakHeight',5);
+        [pks_5toe, locs_5mt] = findpeaks(walk_trial.CS2(10:end), 'MinPeakDistance',20, 'MinPeakHeight',5);
+        [pks_1mtp, locs_1mt] = findpeaks(walk_trial.CS3(10:end), 'MinPeakDistance',20, 'MinPeakHeight',5);
+        [pks_nav, locs_nav] = findpeaks(walk_trial.CS4(10:end), 'MinPeakDistance',20, 'MinPeakheight',5);
+        [pks_calc, locs_5mt] = findpeaks(walk_trial.CS5(10:end), 'MinPeakDistance',20, 'MinPeakHeight',5);
+        [pks_hallux, locs_1mt] = findpeaks(walk_trial.CS6(10:end), 'MinPeakDistance',20);
+        [pks_cub, locs_calc] = findpeaks(walk_dat.CS7(10:end), 'MinPeakDistance',20, 'MinPeakheight',5);
+        
+        % Outlier detection %
+        pks_1mtp = pks_1mtp(pks_1mtp > (0.55 * mean(pks_1mtp)));
+        pks_5mtp = pks_5mtp(pks_5mtp > (0.55 * mean(pks_5mtp)));
+        pks_calc = pks_calc(pks_calc > (0.55 * mean(pks_calc)));
+        pks_nav = pks_nav(pks_nav > (0.55 * mean(pks_nav)));
+        
+        %%%%% Calculate the averages, etc. %%%
+        longdata(1).avg_nav{counter} = mean(pks_nav);
+        longdata(1).avg_cub{counter} = mean(pks_cub);
+        longdata(1).avg_5mtp{counter} = mean(pks_5mtp);
+        longdata(1).avg_1mtp{counter} = mean(pks_1mtp);
+        longdata(1).avg_heel{counter} = mean(pks_calc);
+        longdata(1).cvHeel{counter} = (std(walk_dat.CS5(10:end)) / mean(walk_dat.CS5(10:end))) * 100;
+        
+        %%% Save TS data %%%
+        longdata(1).locs{counter} = locs_calc;
+        longdata(1).calc{counter} = walk_trial.CS5;
+        longdata(1).mtp1{counter} = walk_trial.CS3;
+        longdata(1).mtp5{counter} = walk_trial.CS1;
+        longdata(1).nav{counter} = walk_trial.CS4;
+        longdata(1).cub{counter} = walk_trial.CS7;
+        
+        longdata(1).pkCalc{counter} = pks_calc;
+        longdata(1).pk5mt{counter} = pks_5mtp;
+        longdata(1).pk1mt{counter} = pks_1mtp;
+        longdata(1).pkCub{counter} = pks_nav;
+        longdata(1).pkNav{counter} = pks_nav;
+        longdata(1).config{counter} = file.name;
+        
+        counter = counter + 1;
+    end
 end
 
 %%%% Bar plot with error bars for averages %%%%
@@ -120,7 +120,9 @@ err.LineStyle = 'none';
 hold off
 
 %% 
-for i = 1:5
-   plot(longdata.mtp5{1,i})
-   hold on
-end
+% for i = 1:5
+%    plot(longdata.nav{1,i})
+%    hold on
+% end
+
+aa_results = [longdata.avg_1mtp; longdata.avg_5mtp; longdata.avg_nav; longdata.avg_heel]
--- convLeftVals.m
+++ convLeftVals.m
@@ -20,17 +20,17 @@ A3cal = 8.42*10^9; B3cal = 1.99; C3cal = -1.65*10^4; D3cal = 7.43*10^2;
 A4cal = 1.54*10^9; B4cal = 1.72; C4cal = -1.82*10^4; D4cal = 7.21*10^2;
 A5cal = 6.27*10^12; B5cal = 3.13; C5cal = -8.43*10^3; D5cal = 6.76*10^2;
 A6cal = 3.94*10^10; B6cal = 2.31; C6cal = -1.38*10^4; D6cal = 6.27*10^2; 
-A7cal = 1.51*10^9; B7cal = 1.57; C7cal = -3.99*10^4; D7cal = 8.41^10^2;
+A7cal = 1.51*10^9; B7cal = 1.57; C7cal = -3.99*10^4; D7cal = 8.41*10^2;
 
 
-convValues.CS0 = (A0cal * ((data.S0(2:end) - (mean(calibData.S0) - D0cal)).^-B0cal) + C0cal) * calFactor;
-convValues.CS1 = (A1cal * ((data.S1(2:end) - (mean(calibData.S1) - D1cal)).^-B1cal) + C1cal) * calFactor;
-convValues.CS2 = (A2cal * ((data.S2(2:end) - (mean(calibData.S2) - D2cal)).^-B2cal) + C2cal) * calFactor;
-convValues.CS3 = (A3cal * ((data.S3(2:end) - (mean(calibData.S3) - D3cal)).^-B3cal) + C3cal) * calFactor;
-convValues.CS4 = (A4cal * ((data.S4(2:end) - (mean(calibData.S4) - D4cal)).^-B4cal) + C4cal) * calFactor;
-convValues.CS5 = (A5cal * ((data.S5(2:end) - (mean(calibData.S5) - D5cal)).^-B5cal) + C5cal) * calFactor;
-convValues.CS6 = (A6cal * ((data.S6(2:end) - (mean(calibData.S6) - D6cal)).^-B6cal) + C6cal) * calFactor;
-convValues.CS7 = (A7cal * ((data.S7(2:end) - (mean(calibData.S7) - D7cal)).^-B7cal) + C7cal) * calFactor;
+convValues.CS0 = (A0cal * ((data.S0(2:end) - (mean(calibData.S0(30:50)) - D0cal)).^-B0cal) + C0cal) * calFactor;
+convValues.CS1 = (A1cal * ((data.S1(2:end) - (mean(calibData.S1(30:50)) - D1cal)).^-B1cal) + C1cal) * calFactor;
+convValues.CS2 = (A2cal * ((data.S2(2:end) - (mean(calibData.S2(30:50)) - D2cal)).^-B2cal) + C2cal) * calFactor;
+convValues.CS3 = (A3cal * ((data.S3(2:end) - (mean(calibData.S3(30:50)) - D3cal)).^-B3cal) + C3cal) * calFactor;
+convValues.CS4 = (A4cal * ((data.S4(2:end) - (mean(calibData.S4(30:50)) - D4cal)).^-B4cal) + C4cal) * calFactor;
+convValues.CS5 = (A5cal * ((data.S5(2:end) - (mean(calibData.S5(30:50)) - D5cal)).^-B5cal) + C5cal) * calFactor;
+convValues.CS6 = (A6cal * ((data.S6(2:end) - (mean(calibData.S6(30:50)) - D6cal)).^-B6cal) + C6cal) * calFactor;
+convValues.CS7 = (A7cal * ((data.S7(2:end) - (mean(calibData.S7(30:50)) - D7cal)).^-B7cal) + C7cal) * calFactor;
 
 
 end
